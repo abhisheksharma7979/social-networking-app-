cdf5e32396faf1904a46db84537b0c875ab1a7d8
Abhishek <abhishek.sharma@anchanto.com>
2021-10-13 14:34:59 +0530

    last

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_080930) do
+ActiveRecord::Schema.define(version: 2021_10_13_084851) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+    t.integer "blogger_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
